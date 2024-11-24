1faaefe9b85aebfe10d7cf1e36f4f41f78e559d6
juanpablopos <juan_posada82211@elpoli.edu.co>
2024-11-24 00:57:09 -0500

    Avance al conectar informacion en la tabla del modulo de ventas

---
+++ components/Modal/Modal.ventas.php
@@ -0,0 +1,22 @@
+<div class="content-modal" style="display: none;" >
+    <div class="content-modal-container">
+        <div class="content-form">
+            <div class="header-modal">
+            <div class="content-title"></div>
+        </div>
+        <div class="content-info">
+            <div class="modal-venta">
+                <div class="content-title">
+                    <div class="title">Confirmar Venta</div>
+                </div>
+                <input type="text" class="total-field" value="$0.00" readonly>
+                <p class="question">¿Esta seguro que desea procesar esta venta?</p> 
+            </div>
+        </div>
+            <div class="content-buttons">
+                <div class="btn-modal button-cancel" onclick='toggleModal()'>Cancelar</div>
+                <div class="btn-modal button-confirm">Aceptar</div>
+            </div>
+        </div>
+    </div>
+</div>
