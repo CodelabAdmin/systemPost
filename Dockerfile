0b02e7fa0932210447c5ee6b18c840807e1c9e10
Sebastian Guerra <149134186+SebastianDevps@users.noreply.github.com>
2024-12-01 13:44:36 -0500

    Ajustes de despliegue

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM php:8.1-apache
 
 # Instalar extensiones PHP necesarias
-RUN docker-php-ext-install pdo pdo_mysql
+RUN docker-php-ext-install pdo pdo_mysql mysqli
 
 # Habilitar mod_rewrite
 RUN a2enmod rewrite
@@ -10,4 +10,8 @@ RUN a2enmod rewrite
 COPY . /var/www/html/
 
 # Establecer permisos
-RUN chown -R www-data:www-data /var/www/html 
+RUN chown -R www-data:www-data /var/www/html
+
+# Limpiar el cache de apache
+RUN a2enmod headers
+RUN service apache2 restart
--- api/index.php
+++ api/index.php
@@ -1,12 +1,11 @@
-
-<?php 
-    require 'core/app.php';
-    $AppRoutes = new AppRoutes;
-    require 'routes/routes.php';
-    require 'headers.php';
-    require 'db/conexion.php';
-    $listRoutes=$AppRoutes->getRoutes();
-    $AppResponse = new AppResponse($listRoutes);
-    $AppResponse->loadResponse();
+<?php
+ob_start();
+require 'headers.php';
+require 'core/app.php';
+$AppRoutes = new AppRoutes;
+require 'routes/routes.php';
+require 'db/conexion.php';
+$listRoutes=$AppRoutes->getRoutes();
+$AppResponse = new AppResponse($listRoutes);
+$AppResponse->loadResponse();
 ?>
-  
