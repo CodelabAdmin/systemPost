f90820581abc92874128e1dae5f076bf97328a7b
YedixonRDev <yedixonjrf@gmail.com>
2024-12-12 03:20:14 -0500

    modulo de reportes

---
+++ pages/page.reportes.php
@@ -0,0 +1,32 @@
+<div class="page-reportes">
+   <div class="content-info-counter-reportes">
+      <div class="content-info-reportes">
+         <div class="content-title-reportes">
+               Reportes
+         </div>
+         <div class="content-description-reportes">
+            El módulo de reportes facilita la consulta de reportes de las ventas en el sistema mediante una tabla que permite filtrarlos y ver detalles en un modal. Está enfocado solo en la visualización y control del reportes. 
+         </div>
+      </div> 
+      <div class="content-counter-reportes">
+         <div class="info-counter-reportes">
+            <div class="counter-icon-reportes">icon</div>
+            <div class="counter-number-reportes">30</div>
+         </div>
+         <div class="counter-title-reportes">Cantidad de Reportes</div>
+      </div>
+   </div>
+   <div class="content-search-reportes">
+      <div class="content-add-reportes">+</div>
+      <div class="content-btn-add-reportes">Nuevo Reporte</div>
+      <input class="search-reportes-input" type="text" placeholder="Buscar"/>
+      <div class="btn-search-reportes">.</div>
+      <div class="icon-reportes">icon</div>
+   </div>
+   
+   <div class="container-table-reportes">
+      <div class="table-reportes">
+          <?php require('./components/Table/Table.reportes.php') ?> 
+      </div> 
+   </div>
+</div>
